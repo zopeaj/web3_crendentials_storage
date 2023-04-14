d0fd117222017b415ac365a919afab44bab3a850
Ezekiel <zopeaj@gmail.com>
2023-04-14 11:40:29 +0100

    update file

---
+++ backend/app/Credentials.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+contract Credentials {
+  string public data;
+
+  constructor(string memory _data) public {
+    data = _data
+  }
+}
---
+++ backend/app/deploy_crendentails_contract.py
@@ -0,0 +1,1 @@
+from web2 import Web3
